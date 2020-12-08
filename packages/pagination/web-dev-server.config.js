c46fe7794606b5f4eaaf2319b44e704be962991f
Dang Van Thanh <dangvanthanh@dangthanh.org>
2020-12-08 22:42:07 +0700

    Update dev-server with monorepo

---
+++ packages/pagination/web-dev-server.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  open: true,
+  nodeResolve: true,
+  watch: true,
+  appIndex: 'packages/pagination/index.html',
+  // in a monorepo you need to set set the root dir to resolve modules
+  rootDir: '../../',
+};
